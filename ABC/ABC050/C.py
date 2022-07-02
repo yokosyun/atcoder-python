8c452438a28c69da384832981e564e599229fcd3
yokoko <yoko.syun.1408@gmail.com>
2022-07-02 17:36:48 +0900

    update ABC50 C

--- ABC/ABC050/C.py
+++ ABC/ABC050/C.py
@@ -1,39 +1,12 @@
 from collections import Counter
 
-
-def is_correct(count_dict, is_even):
-    for key, val in count_dict.items():
-        if is_even:
-            if key == 1:
-                if val != 2:
-                    return 0.0
-            elif key % 2 == 1:
-                if val % 2 != 0:
-                    return 0.0
-            else:
-                return 0
-        else:
-            if key == 0:
-                if val != 1:
-                    return 0.0
-            elif key % 2 == 0:
-                if val % 2 != 0:
-                    return 0.0
-            else:
-                return 0.0
-
-    return (2 ** (n // 2)) % (10 ** 9 + 7)
-
-
-if __name__ == "__main__":
-
-    n = int(input())
-    a = list(map(int, input().split()))
-    count_dict = Counter(a)
-
-    is_even = False
-    if n % 2 == 0:
-        is_even = True
-
-    res = is_correct(count_dict, is_even)
-    print(int(res))
+n = int(input())
+a = list(map(int, input().split()))
+c = Counter(a)
+d = list(c.values())
+if (n % 2 == 1 and c[0] == 1 and d.count(2) == (n - 1) // 2) or (
+    n % 2 == 0 and d.count(2) == n // 2
+):
+    print((2 ** (n // 2)) % (10 ** 9 + 7))
+else:
+    print(0)
