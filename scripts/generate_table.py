450bd65554f500a543fb46476226617b614283c1
yokoko <yoko.syun.1408@gmail.com>
2022-09-01 21:32:01 +0900

    add generate_table

---
+++ scripts/generate_table.py
@@ -0,0 +1,29 @@
+import argparse
+import glob
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--root_path", type=str, default="ABC")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    file_paths = sorted(glob.glob(args.root_path + "/*/*.py"))
+
+    pre_folder = ""
+    line = ""
+    for file_path in file_paths:
+        folder = file_path.split("/")[-2]
+
+        if pre_folder != folder:
+            print(line)
+            line = "|" + folder + "|"
+            pre_folder = folder
+
+        line += "[&check;](" + file_path + ")|"
+
+
+if __name__ == "__main__":
+    main()
